5b6b318c406480abad2127b4c7fc5409859e103b
logixkjy <logixkjy@naver.com>
2026-01-01 12:12:24 +0900

    [init] Initialize project with unit test target

---
+++ ITBooksSearch/Presentation/BookList/BookListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BookListViewController.swift
+//  ITBooksSearch
+//
+//  Created by JooYoung Kim on 1/1/26.
+//
+
+import UIKit
+
+class BookListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
